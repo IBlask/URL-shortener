d31b3c2adb049f1335b061547c166fb94622cda3
IBlask <95757169+IBlask@users.noreply.github.com>
2023-05-09 14:44:48 +0200

    Napravljen API za login

--- src/main/java/com/shorty/shorty/dto/request/RequestLogin.java
+++ src/main/java/com/shorty/shorty/dto/request/RequestLogin.java
@@ -1,5 +1,57 @@
 package com.shorty.shorty.dto.request;
 
+import com.shorty.shorty.entity.User;
+import com.shorty.shorty.repository.UserRepository;
+
 public class RequestLogin {
     //TODO RequestLogin
+    private String accountID;
+    private String password;
+
+    public RequestLogin() {
+        super();
+    }
+
+    public RequestLogin(String accountID, String password) {
+        this.accountID = accountID;
+        this.password = password;
+    }
+
+    public String getAccountID() {
+        return accountID;
+    }
+
+    public void setAccountID(String accountID) {
+        this.accountID = accountID;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEmpty() {
+        return accountID == null && password == null;
+    }
+
+    public boolean accountIdIsBlank() {
+        return accountID.isBlank() || password == null;
+    }
+
+    public boolean passwordIsBlank() {
+        return password.isBlank() || password == null;
+    }
+
+    public boolean accountIdAndPasswordAreMatching(UserRepository userRepository) {
+        User user = userRepository.findByUsername(this.accountID);
+
+        if ((user != null) && user.getPassword().equals(this.password)) {
+            return true;
+        }
+
+        return false;
+    }
 }
--- src/main/java/com/shorty/shorty/dto/response/ResponseLogin.java
+++ src/main/java/com/shorty/shorty/dto/response/ResponseLogin.java
@@ -2,4 +2,13 @@ package com.shorty.shorty.dto.response;
 
 public class ResponseLogin {
     //TODO ResponseLogin
+    private boolean success = false;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
 }
