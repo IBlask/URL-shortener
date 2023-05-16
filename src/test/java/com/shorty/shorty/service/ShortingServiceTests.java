0b6e3147d37bd1f51c0847769e4661b1e512984e
IBlask <95757169+IBlask@users.noreply.github.com>
2023-05-16 09:32:08 +0200

    Dodani Unit testovi za ShortingService i ResponseShort

--- src/test/java/com/shorty/shorty/service/ShortingServiceTests.java
+++ src/test/java/com/shorty/shorty/service/ShortingServiceTests.java
@@ -5,11 +5,15 @@ import com.shorty.shorty.dto.response.ResponseShort;
 import com.shorty.shorty.repository.UrlRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -19,20 +23,52 @@ public class ShortingServiceTests {
 
     @Autowired
     ShortingService shortingService;
-    @Autowired
+    @Mock
     UrlRepository urlRepository;
 
     @Test
     public void short_test_goodRequest() {
-
-
         RequestShort request = mock(RequestShort.class);
         request.setUrl("https://www.google.com/");
         when(request.isEmpty()).thenReturn(false);
         when(request.urlIsBlank()).thenReturn(false);
 
-        ResponseShort responseShort = shortingService.shorting(request, urlRepository);
+        ResponseShort response = shortingService.shorting(request, urlRepository);
 
-        assertNotNull(responseShort);
+        String shortUrl = response.getShortUrl();
+        String shortUrlId = shortUrl.substring(shortUrl.length() - 5);
+        Pattern p = Pattern.compile("[a-z]{5}");
+        Matcher m = p.matcher(shortUrlId);
+        boolean b = m.matches();
+
+        assertTrue(response.getShortUrl().contains("http://localhost:8080/"));
+        assertTrue(b);
+        assertNull(response.getDescription());
     }
+
+    @Test
+    public void short_test_badRequest() {
+        RequestShort request = mock(RequestShort.class);
+        when(request.isEmpty()).thenReturn(true);
+        when(request.urlIsBlank()).thenReturn(false);
+
+        ResponseShort response = shortingService.shorting(request, urlRepository);
+
+        assertEquals("Error occurred! Please try again.", response.getDescription());
+        assertNull(response.getShortUrl());
+    }
+
+    @Test
+    public void short_test_blankRequest() {
+        RequestShort request = mock(RequestShort.class);
+        request.setUrl("");
+        when(request.isEmpty()).thenReturn(false);
+        when(request.urlIsBlank()).thenReturn(true);
+
+        ResponseShort response = shortingService.shorting(request, urlRepository);
+
+        assertEquals("Please enter your URL!", response.getDescription());
+        assertNull(response.getShortUrl());
+    }
+
 }
